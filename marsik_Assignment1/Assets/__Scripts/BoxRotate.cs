fd406d567f3bf77f181fab055d890243a192e429
milosarsik <milosarsic14@gmail.com>
2019-01-22 18:45:23 -0500

    Added Box Colliders

--- marsik_Assignment1/Assets/__Scripts/BoxRotate.cs
+++ marsik_Assignment1/Assets/__Scripts/BoxRotate.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BoxRotate : MonoBehaviour
 {
+    int randomRotationX;              // getting a random value for the x axis
+    int randomRotationZ;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,10 +15,13 @@ public class BoxRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int randomRotationX = Random.Range(-5, 5);              // getting a random value for the x axis
-        int randomRotationZ = Random.Range(-3, 3);              // getting a random value for the y axis
+        randomRotationX = Random.Range(-5, 5);              // getting a random value for the x axis
+        randomRotationZ = Random.Range(-3, 3);              // getting a random value for the y axis
+    }
 
-        // checking is the user has left clicked the mouse
+    private void OnMouseOver()
+    {
+        // checking if the user has left clicked the mouse
         if (Input.GetMouseButtonDown(0))
         {
             gameObject.transform.eulerAngles = new Vector3(randomRotationX, 0, randomRotationZ);        // changing the angle of the box to the random values
