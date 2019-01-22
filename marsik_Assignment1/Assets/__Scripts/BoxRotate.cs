6974b29c628ac722b9f90465b5de01fe465b2f2b
milosarsik <milosarsic14@gmail.com>
2019-01-22 16:01:32 -0500

    Added marsik_Assignment1

---
+++ marsik_Assignment1/Assets/__Scripts/BoxRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int randomRotationX = Random.Range(-5, 5);
+        int randomRotationZ = Random.Range(-3, 3);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.transform.eulerAngles = new Vector3(randomRotationX, 0, randomRotationZ);
+        }
+    }
+}
---
+++ marsik_Assignment1/Assets/__Scripts/ShapeReset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeReset : MonoBehaviour
+{
+    Vector3 originalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // OnMouseOver() is called every frame while the mouse is over the object
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            gameObject.transform.position = originalPos;
+        }
+    }
+}
