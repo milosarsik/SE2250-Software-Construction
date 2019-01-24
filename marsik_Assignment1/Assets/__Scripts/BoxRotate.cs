0f6c590c735fb70612cbe2bb30b4f7deabd1779e
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2019-01-23 23:47:40 -0500

    Update BoxRotate.cs

--- marsik_Assignment1/Assets/__Scripts/BoxRotate.cs
+++ marsik_Assignment1/Assets/__Scripts/BoxRotate.cs
@@ -20,9 +20,9 @@ public class BoxRotate : MonoBehaviour
         // checking if the user has left clicked the mouse
         if (Input.GetMouseButtonDown(0))
         {
-            float randomRotationX = Random.Range(-3, 3);              // getting a random value for the x axis
-            float randomRotationZ = Random.Range(-3, 3);              // getting a random value for the z axis
-            float randomRotationY = Random.Range(-3, 3);              // getting a random value for the y axis
+            float randomRotationX = Random.Range(-3, 3);              // getting a random value for the x-axis
+            float randomRotationZ = Random.Range(-3, 3);              // getting a random value for the z-axis
+            float randomRotationY = Random.Range(-3, 3);              // getting a random value for the y-axis
 
             gameObject.transform.eulerAngles = new Vector3(randomRotationX, randomRotationY, randomRotationZ);        // changing the angle of the box to the random values
         }
