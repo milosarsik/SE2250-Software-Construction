7ff8b63df8198f2bd4ab4e01869f4e00d3ad8f5a
Milos Arsik <marsik@uwo.ca>
2019-03-18 13:31:13 -0400

    final edit

---
+++ marsik_Assignment3/Prospector Solitaire-Broken/Assets/__Scripts/ShareHighScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ShareHighScore : ScoreManager
+{
+    private string _URL = "https://twitter.com/intent/tweet";
+    private string _language = "en";
+    private string _param = " ";
+    private string _display = ("score" + SCORE);
+    public Button shareButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button mediaButton = shareButton.GetComponent<Button>();
+        mediaButton.onClick.AddListener(OnClickTask);
+
+    }
+
+    void OnClickTask()
+    {
+        Application.OpenURL(_URL + "?text= My high score is: " + HIGH_SCORE + "&amp;lang=" + UnityWebRequest.EscapeURL(_language));
+    }
+}
